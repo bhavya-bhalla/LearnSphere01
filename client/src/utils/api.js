c6c4f9327ad961f3644ba91679ab0e9f3b7b4e09
BHAVYA BHALLA <138828284+bhavya-bhalla@users.noreply.github.com>
2025-11-02 15:57:23 +0530

    Hardcode Render backend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-console.log('API BASE URL:', API_BASE_URL)
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = 'https://learnsphere01.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
